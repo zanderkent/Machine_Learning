36477f3e71078d189cbb6cbe2dc3509ff1ae0078
Zander Kent <zanderjordankent@gmail.com>
2019-07-10 20:18:50 -0700

    Scrubbing custom eval

--- Advanced_Bayesian/custom_eval_functions.py
+++ Advanced_Bayesian/custom_eval_functions.py
@@ -2,26 +2,11 @@ import pandas as pd
 import numpy as np
 
 class custom_accuracy():
+    """This is a custom accuracy function that was written for the purpose of this project, 
+       I have removed all information here, but this class is still useful, since you can do the same thing here
+       and write your own custom function if you want.
+    """
     def GetPrediction(model,X,full_data,labels):
-        X_prob=model.predict(X)
-        classes = labels.columns
-        Xdf=pd.DataFrame(X_prob,index=X.index,columns=classes)
-        def n_largest(c):
-            large = c.nlargest(6)
-            return pd.DataFrame([large.index.tolist()+large.values.tolist()]).loc[0]
-        df=Xdf.apply(lambda c: n_largest(c),axis=1)
-        df.columns = ['Prediction1','Prediction2','Prediction3','Prediction4','Prediction5','Prediction6','Confidence1','Confidence2','Confidence3','Confidence4','Confidence5','Confidence6']
-        df['next_index']=full_data.loc[X.index.values]['next_index']
         return df
     def suppresed_top_accuracy(preds):
-        preds['Predictions'] = preds['Prediction1'] + ':' + preds['Prediction2'] + ':' + preds['Prediction3'] + ':' + preds['Prediction4'] + ':' + preds['Prediction5'] + ':' + preds['Prediction6']
-        preds['Predictions'] = preds['Predictions'].str.split(':').apply(lambda x: list(filter(lambda y: y not in ['General Inquiries','Other','Cancel Card/Account'], x)))
-        preds['top_3'] = preds['Predictions'].str[:3]
-        counts_3 = []
-        counts_1 = []
-        for each in preds.index:
-            actual = preds['actual_action_list'].iloc[each]
-            top_3_preds = preds.top_3.iloc[each]
-            counts_3.append(bool(set(actual) & set(top_3_preds)))
-            counts_1.append(top_3_preds[0] in actual)
         return(np.array(counts_3), np.array(counts_1))
